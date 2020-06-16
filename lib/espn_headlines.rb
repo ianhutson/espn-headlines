593c374d5a5e53c2886942c74f6b33d19fd94804
Ian Hutson <ih00209@georgiasouthern.edu>
2020-06-16 18:27:46 -0400

    cleaned up

--- lib/espn_headlines.rb
+++ lib/espn_headlines.rb
@@ -2,7 +2,6 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 
-
 require_relative "./espn_headlines/version"
 require_relative './espn_headlines/cli'
 require_relative './espn_headlines/headline'
